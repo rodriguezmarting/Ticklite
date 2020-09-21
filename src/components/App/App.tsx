104042394d0d0b7e1f8d61242b09a83d691baba5
rodriguezmarting <martin.rg25@hotmail.com>
2020-09-21 16:21:56 -0300

    ChakraUI configured.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { customTheme } from "../../theme";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">Hi</Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={customTheme}>
+      <CSSReset />
+      <Router>
+        <Switch>
+          <Route path="/">Hi</Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
 
