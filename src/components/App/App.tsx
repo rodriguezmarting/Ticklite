783751a4afee0b609dd2d94eea5797cc8e93c194
rodriguezmarting <martin.rg25@hotmail.com>
2020-09-21 15:34:57 -0300

    React router configured.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">Hi</Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
