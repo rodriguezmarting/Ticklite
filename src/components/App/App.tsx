42abccb3f8b51aba976b199c14a33ffb32b57b92
rodriguezmarting <martin.rg25@hotmail.com>
2020-09-22 16:00:27 -0300

    CSSReset config. Dark background by default.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { ThemeProvider, CSSReset, ITheme } from "@chakra-ui/core";
 import { customTheme } from "../../theme";
 import Home from "../Home/Home";
+
+// This app will be dark by default for now
+// both modes will be the same
+const config = (theme: ITheme) => ({
+  light: {
+    color: theme.colors.whiteAlpha[900],
+    bg: theme.colors.gray[800],
+    borderColor: theme.colors.whiteAlpha[300],
+    placeholderColor: theme.colors.whiteAlpha[400],
+  },
+  dark: {
+    color: theme.colors.whiteAlpha[900],
+    bg: theme.colors.gray[800],
+    borderColor: theme.colors.whiteAlpha[300],
+    placeholderColor: theme.colors.whiteAlpha[400],
+  },
+});
 
 const App: React.FC<{}> = () => {
   return (
     <ThemeProvider theme={customTheme}>
-      <CSSReset />
+      <CSSReset config={config} />
       <Router>
         <Switch>
           <Route path="/" component={Home} />
