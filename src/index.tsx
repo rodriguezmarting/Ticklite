702b912d0b344999b8201b694a70d697713eaa80
rodriguezmarting <martin.rg25@hotmail.com>
2020-10-22 14:56:05 -0300

    Removed outline when not using keyboard.

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,14 @@ import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import { store } from "./ducks/store";
 import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
-import { customTheme, config } from "./shared/theme";
+import { customTheme, config, GlobalStyles } from "./shared/theme";
+import { Global } from "@emotion/core";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store()}>
       <ThemeProvider theme={customTheme}>
+        <Global styles={GlobalStyles} />
         <CSSReset config={config} />
         <App />
       </ThemeProvider>
--- src/shared/theme.tsx
+++ src/shared/theme.tsx
@@ -1,4 +1,17 @@
 import { theme, ITheme } from "@chakra-ui/core";
+import "focus-visible/dist/focus-visible";
+import { css } from "@emotion/core";
+
+export const GlobalStyles = css`
+  /*
+    This will hide the focus indicator if the element receives focus    via the mouse,
+    but it will still show up on keyboard focus.
+  */
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+    outline: none;
+    box-shadow: none;
+  }
+`;
 
 export const customTheme = {
   ...theme,
