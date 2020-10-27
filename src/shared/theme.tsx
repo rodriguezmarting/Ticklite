c367377dc6085bbf175276a7f5b8e66e23932b2f
rodriguezmarting <martin.rg25@hotmail.com>
2020-10-27 16:08:12 -0300

    Small comment fixed.

--- src/shared/theme.tsx
+++ src/shared/theme.tsx
@@ -4,7 +4,7 @@ import { css } from "@emotion/core";
 
 export const GlobalStyles = css`
   /*
-    This will hide the focus indicator if the element receives focus    via the mouse,
+    This will hide the focus indicator if the element receives focus via the mouse,
     but it will still show up on keyboard focus.
   */
   .js-focus-visible :focus:not([data-focus-visible-added]) {
