c38beb4de1d3e4d68d6a78d97f3a29e4fe4f7dc3
rodriguezmarting <martin.rg25@hotmail.com>
2020-11-10 16:08:47 -0300

    Layout icons for creating new questions added.

--- src/shared/theme.tsx
+++ src/shared/theme.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { theme, ITheme } from "@chakra-ui/core";
 import "focus-visible/dist/focus-visible";
 import { css } from "@emotion/core";
@@ -13,6 +14,44 @@ export const GlobalStyles = css`
   }
 `;
 
+const customIcons = {
+  layout4: {
+    path: (
+      <g fill="currentColor">
+        <path
+          fillRule="evenodd"
+          d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"
+        />
+      </g>
+    ),
+    viewBox: "0 0 16 16",
+  },
+  layout3: {
+    path: (
+      <g fill="currentColor">
+        <path
+          fillRule="evenodd"
+          d="M0 2.5A1.5 1.5 0 0 1 1.5 1h13A1.5 1.5 0 0 1 16 2.5v11a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-11zM1.5 2a.5.5 0 0 0-.5.5v11a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-11a.5.5 0 0 0-.5-.5h-13z"
+        />
+        <path fillRule="evenodd" d="M5 15V1h1v14H5zm5 0V1h1v14h-1z" />
+      </g>
+    ),
+    viewBox: "0 0 16 16",
+  },
+  layout2: {
+    path: (
+      <g fill="currentColor">
+        <path
+          fillRule="evenodd"
+          d="M14 2H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zM2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z"
+        />
+        <path fillRule="evenodd" d="M7.5 14V2h1v12h-1z" />
+      </g>
+    ),
+    viewBox: "0 0 16 16",
+  },
+};
+
 export const customTheme = {
   ...theme,
   colors: {
@@ -27,6 +66,10 @@ export const customTheme = {
     body: "Montserrat, sans-serif",
     heading: "Montserrat, sans-serif",
     mono: "Montserrat, sans-serif",
+  },
+  icons: {
+    ...theme.icons,
+    ...customIcons,
   },
 };
 
